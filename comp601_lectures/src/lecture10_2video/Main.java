12d718af698a534aa287569d600c195adc3364a9
monchan-new <monstertraveler@gmail.com>
2025-08-18 17:29:39 +1200

    Finish video 10

---
+++ comp601_lectures/src/lecture10_2video/Main.java
@@ -0,0 +1,113 @@
+package lecture10_2video;
+
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    // Polymmorphism via inheritance
+    List<Mammal> mammals = new LinkedList<>();
+    Mammal m = new Whale(3000);
+    mammals.add(m);
+    m = new Seal(100);
+    mammals.add(m);
+    m = new Lion(120);
+    mammals.add(m);
+    m = new Cheetah(50);
+    mammals.add(m);
+    for (Mammal mml : mammals) {
+      System.out.println(mml.getWeigth());
+    }
+
+    // LandBound xx = new LandBound();//interface can't instantiate
+    // Polymorphism via LandBound interface
+    List<LandBound> landAnims = new LinkedList<>();
+    LandBound la = new Lion(120);
+    landAnims.add(la);
+    la = new Cheetah(50);
+    landAnims.add(la);
+    la = new Seal(100);
+    landAnims.add(la);
+    for (LandBound lb : landAnims) {
+      lb.run();
+    }
+    // Ploymorphism via SeaBound interface
+    List<SeaBound> seaAnims = new LinkedList<>();
+    SeaBound sa = new Whale(3800);
+    seaAnims.add(sa);
+    sa = new Seal(100);
+    seaAnims.add(sa);
+    for (SeaBound sb : seaAnims) {
+      sb.swim();
+    }
+
+  }
+}
+
+class Mammal {
+  private double weigth;
+
+  public Mammal(double weigth) {
+    this.setWeigth(weigth);
+  }
+
+  public double getWeigth() {
+    return this.weigth;
+  }
+
+  public void setWeigth(double weigth) {
+    this.weigth = weigth;
+  }
+
+}
+
+interface LandBound {
+  void run();// implictly means "public abstract"
+}
+
+interface SeaBound {
+  void swim();
+}
+
+// class Whale extends Mammal implements SeaBound, OtherInterface
+class Whale extends Mammal implements SeaBound {
+  public Whale(double weigth) {
+    super(weigth);
+  }
+
+  public void swim() {
+    System.out.println("Whale swimming...");
+  }
+}
+
+class Seal extends Mammal implements SeaBound, LandBound {
+  public Seal(double weigth) {
+    super(weigth);
+  }
+
+  public void swim() {
+    System.out.println("Seal swimming...");
+  }
+  public void run() {
+    System.out.println("Seal running...");
+  }
+}
+
+class Lion extends Mammal implements LandBound {
+  public Lion(double weigth) {
+    super(weigth);
+  }
+
+  public void run() {
+    System.out.println("Lion running...");
+  }
+}
+
+class Cheetah extends Mammal implements LandBound {
+  public Cheetah(double weigth) {
+    super(weigth);
+  }
+
+  public void run() {
+    System.out.println("Cheetah running...");
+  }
+}
--- comp601_lectures/src/lecture10video/Main.java
+++ comp601_lectures/src/lecture10video/Main.java
@@ -1,7 +1,123 @@
 package lecture10video;
 
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
   public static void main(String[] args) {
     
+    List<Employee> emps = new LinkedList<>();
+    //Employee e = new Employee("xx", 10, 10);Can't instantiate abstract class
+    Employee e = new Salesperson("S11", 40000, 10);
+    emps.add(e);
+    e = new CEO("C11", 100000, 100);//re-use the pointer reference varable
+    emps.add(e);
+    e = new CEO("C22", 120000, 50);
+    emps.add(e);
+    e = new Salesperson("S22", 45000, 15);
+    emps.add(e);
+    // List<Employee> emps = new ArrayList<>(Arrays.asList(
+    //   new Salesperson("S11", 40000, 10),
+    //   new CEO("C11", 100000, 100),
+    //   new CEO("C22", 120000, 50),
+    //   new Salesperson("S22", 45000, 15)
+    // ));
+    for(Employee emp : emps) {
+      emp.displayInfo();
+      emp.raiseSalary();
+    } 
+    
+      System.out.println();
+    for(Employee emp : emps) {
+      emp.displayInfo();
+    }
   }
+}
+
+//abstract class
+abstract class Employee {
+  protected String EID;
+  protected double salary;
+
+  public Employee(String eid, double salary) {
+    this.setEid(eid);
+    this.setSalary(salary);
+  }
+
+  public void displayInfo() {
+    System.out.printf("Employee ID: %s, Employee salary: %f\n", this.getEid(), this.getSalary());
+  }  
+
+  //abstract method
+  public abstract void raiseSalary();
+
+  
+  public String getEid() {
+    return this.EID;
+  }
+  public void setEid(String eid) {
+    this.EID = eid;
+  }
+  public double getSalary() {
+    return this.salary;
+  }
+  public void setSalary(double salary) {
+    this.salary = salary;
+  }
+
+}
+
+class Salesperson extends Employee {
+  private int salesNumber;
+
+  public Salesperson(String id, double salary, int salesNumber) {
+    super(id, salary);
+    this.setSalesNumber(salesNumber);
+  }
+
+  @Override public void raiseSalary() {
+    double payRiseFactor = 0;
+    if(salesNumber > 50) {
+      payRiseFactor = 20;
+    } else {
+      payRiseFactor = 10;
+    }
+    double rise = salary * payRiseFactor / 100;
+    salary += rise;
+    
+  }
+
+  
+  public int getSalesNumber() {
+    return this.salesNumber;
+  }
+  public void setSalesNumber(int salesNumber) {
+    this.salesNumber = salesNumber;
+  }
+
+}
+
+class CEO extends Employee {
+  private int stockOptions;
+
+  public CEO(String id, double sal, int stockOptions) {
+    super(id, sal);
+    this.stockOptions = stockOptions;
+  }
+
+  @Override public void raiseSalary() {
+    salary += 20000;
+    stockOptions += ThreadLocalRandom.current().nextDouble(1,100);
+  }
+  @Override public void displayInfo() {
+    System.out.printf("Employee ID: %s, Employee salary: %f, Employee stockoptions: %d\n", this.getEid(), this.getSalary(), this.getStockOptions());
+  }  
+
+  public int getStockOptions() {
+    return this.stockOptions;
+  }
+  public void setStockOptions(int stockOptions) {
+    this.stockOptions = stockOptions;
+  }
+
 }
